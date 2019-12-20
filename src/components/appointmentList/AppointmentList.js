12a12aaf8aad70e1f3ca5f224ff09f0be98406d4
omniboyOk <kachorritou@gmail.com>
2019-12-20 19:16:14 -0300

    Added conditional confirmation on the appointment list

--- src/components/appointmentList/AppointmentList.js
+++ src/components/appointmentList/AppointmentList.js
@@ -2,9 +2,11 @@ import React from "react";
 import AppointmentCard from "../appointmentCard/AppointmentCard.js";
 
 const AppointmentList = ({ reservas, deleteAppointment }) => {
+  //generamos un mensaje segun si hay reservas o no
+  const mensaje = reservas.length === 0 ? "Reserva un turno para verlo aquí" : "Administra tus turnos"
   return (
     <div className="card mt-5 mb-5">
-      <h2 className="card-header text-center">Administrar Turnos</h2>
+      <h2 className="card-header text-center">{mensaje}</h2>
       <div className="card-body">
         <div>
           {reservas.map(reserva => {
